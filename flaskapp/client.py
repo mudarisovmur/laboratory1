18639d4b6d3af9ac5b0e410ed9bb3b295fbb840a
mudarisovmur <mudarisovmur@gmail.com>
2023-04-08 12:09:01 +0700

    2.10.2

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -7,12 +7,12 @@ img_data = None
 path = os.path.join('./static','image0008.png')
 # читаем файл и енкодируем его в строку base64
 with open(path, 'rb') as fh:
-    img_data = fh.read()
-    b64 = base64.b64encode(img_data)
+  img_data = fh.read()
+  b64 = base64.b64encode(img_data)
 # создаем json словарь, который
 # отправляется на сервер в виде json-строки
 # преобразование делает сама функция отправки запроса post
 jsondata = {'imagebin':b64.decode('utf-8')}
 res = requests.post('http://localhost:5000/apinet', json=jsondata)
 if res.ok:
-    print(res.json()) 
+  print(res.json()) 
